46aeb7ce465d8e66d5ee03b3427b94abff155ff3
Neja Dolinar <n.dolinar@sportradar.com>
2016-12-01 12:30:18 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+(function(){
+  $(window).scroll(function(e){
+    parallax();
+  });
+  function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('top',-(scrolled*0.2)+'px');
+  }
+})();
